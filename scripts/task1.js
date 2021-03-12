4c8514e0136662469d6682e249f0793b15b1ae94
Sam Dugdale <dugdalewebdev@gmail.com>
2021-03-12 01:10:41 +0000

    added calculate program, bonus and 2nd task output
    1. added calculate program to calculate pre-tip total and tip percentage
    2. added bonus to 2 dec point
    3. added extension to output answer
    4. included functional button for extra
    5. added some css in new stylesheet to tidy up page

--- scripts/task1.js
+++ scripts/task1.js
@@ -1,5 +1,20 @@
 
-alert('Hello World'); 
+alert('Welcome to my JS testing'); 
 console.log('Secret Message');
 
-document.write('<h2>Secret message</h2>')
+// document.write('<h2>Secret message</h2>')
+
+const bills = 34;
+const tips = (bills * 22) / 100;
+
+const totals = (bills + tips).toFixed(2);
+
+
+function myFunction() {
+    document.getElementById("calculate")
+    alert(
+            'Your total bill (£' + bills + '), with tip (£' + tips + '), is £' + totals + ''
+)
+
+  }
+
