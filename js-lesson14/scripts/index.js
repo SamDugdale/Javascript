28cf168e9291ce0282f6f6fc171a5c3af8fa63cf
SamDugdale <dugdalewebdev@gmail.com>
2021-03-30 06:21:19 +0100

    12-16 edit, pseudo edit, header added to js15
    1. index page now links to lessons 12-16
    2. added psuedo to lesson 14 and other tasks
    3. header added to js 15

--- js-lesson14/scripts/index.js
+++ js-lesson14/scripts/index.js
@@ -12,6 +12,7 @@ var shoppingCart=[
         {name:"cheesecake",type:"food",quantity:1,price:4.99},
         {name:"onions",type:"food",quantity:3,price:.4}];
 
+//Task 1 & 2
 //CREATE FUNCTION take arg shoppingCart
 function getTotalPrice (shoppingCart, discountAmount, type) {
     //CREATE VAR totalPrice
@@ -34,17 +35,22 @@ function getTotalPrice (shoppingCart, discountAmount, type) {
     }
 
     //RETURN VAR totalPrice
-    return totalPrice;
+    return totalPrice.toFixed(2);
 };
 
+console.log("Task 1");
 console.log(getTotalPrice(shoppingCart, 20, 'any'));
 
-
+// CREATE FUNCION take arg car, lowPrice, highPrice, quantity
 function getItemsBetweenLowHighPrice (cart, lowPrice, highPrice, quantity) {
+    //CREATE VAR for array
     var arrItems = [];
     var price = 0;
-
+    //LOOP through item
     for (var item of cart) {
+         // USE TERNARY OPERATOR???
+//THEN ADD (item.price * item.quantity : price = item.price) to VAR array
+
         quantity ? price = item.price * item.quantity : price = item.price
 
         if (price >= lowPrice && price <=highPrice) {
@@ -62,37 +68,79 @@ function getItemsBetweenLowHighPrice (cart, lowPrice, highPrice, quantity) {
             arrItems.push(item);
         }
     }  */
-
+        //RETURN var array
         return arrItems;
 }
 
+console.log("Task 2");
 console.log(getItemsBetweenLowHighPrice(shoppingCart, 0.2, 1, true));
 
 
 var numberArray = [3,6,1,68,23,15,3,9,56,10];
 
-function mean (numberArray) {
-    return numberArray.reduce((a, b)=> a + b, 0) / numberArray.length;
-}
-
-console.log(mean(numberArray));
-
-
-function median (numberArray) {
+function mean(numberArray) {
+    var total = 0, i;
+    for (i = 0; i < numberArray.length; i += 1) {
+        total += numberArray[i];
+    }
+    return total / numberArray.length;
+  
+    //return numberArray.reduce((a, b) => a + b, 0) / numberArray.length;
+  }
+  
+  console.log(mean(numberArray));
+  
+  
+  function median (numberArray) {
     var sortedNumbers = numberArray.sort(function(a, b){return a - b});
-
-    var value = sortedNumbers[sortedNumbers.length / 2 | 0];
-    
-    return value;     
-}
-
-console.log(median(numberArray));
-
-function mode (numberArray) {
-    var modes , count []
-
-
-}
+    var mediumValue = sortedNumbers[sortedNumbers.length / 2 | 0];
+  
+    return mediumValue;
+  }
+  
+  console.log(median(numberArray));
+  
+  function mode (numberArray) {
+    var modes , count = [], i, number, maxIndex = 0;
+   
+      for (i = 0; i < numberArray.length; i += 1) {
+          number = numberArray[i];
+          count[number] = (count[number] || 0) + 1;
+          if (count[number] > maxIndex) {
+              maxIndex = count[number];
+          }
+      }
+   
+      for (i in count)
+          if (count.hasOwnProperty(i)) {
+              if (count[i] === maxIndex) {
+                  modes = Number(i);
+              }
+          }
+   
+      return modes;
+  }
+  
+  console.log(mode(numberArray));
+  
+  
+  function getNumbertype(numberArray, type) {
+    switch (type) {
+      case 'mean' :
+        return mean(numberArray);
+        break;
+      case 'medium' : 
+        return medium(numberArray);
+        break;
+  
+      case 'mode' :
+        return mode(numberArray);
+        break
+  
+      default:
+        return numberArray;
+    }
+  }
 
 console.log(mode(numberArray)); 
 
