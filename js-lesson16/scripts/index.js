42bda8ba6ef65583b66892a118678feb9b83102b
SamDugdale <dugdalewebdev@gmail.com>
2021-03-31 15:00:06 +0100

    corrected display error, pseudo correction
    1. corrected filtering error
    2. added and edited some pseudo

--- js-lesson16/scripts/index.js
+++ js-lesson16/scripts/index.js
@@ -22,51 +22,58 @@ document.addEventListener('click', function (event) {
 });
 */
 
+
 // Execute function and show all columns
-filterSelection("all") 
+filterSelection("all")
+
 function filterSelection(cols) {
-  var x, i;
-  x = document.getElementsByClassName("column");
-  if (cols == "all") cols = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    ttRemoveClass(x[i], "show");
-    if (x[i].className.indexOf(cols) > -1) ttAddClass(x[i], "show");
-  }
+    var show, i;
+    show = document.getElementsByClassName("column");
+    // Display all columns if all class is on
+    if (cols == "all") cols = "";
+    // Add the "show" class (display:block in css) to the filtered elements, while also removing the "show" class from the elements that are not selected
+    for (i = 0; i < show.length; i++) {
+        ttRemoveClass(show[i], "show");
+        // If the column class name is not there remove, if it is show it
+        if (show[i].className.indexOf(cols) > -1) ttAddClass(show[i], "show");
+    }
 }
 
 // Show filtered elements
 function ttAddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
+    // Get element class names
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    // show if container is selected, otherwise display none (column - attribute name)
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
     }
-  }
 }
 
-// Hide elements that are not selected
+// Hide elements that are not the selected button
 function ttRemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
     }
-  }
-  element.className = arr1.join(" ");
+    element.className = arr1.join(" ");
 }
 
-// Add active class to the current button (highlight it)
+
+// Active button class to the current button by highlighting it
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
 }
